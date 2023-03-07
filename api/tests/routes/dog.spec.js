978ec164ff69f9b453ee6e151a605b62d223350e
Eduardocid200 <eduardocid1986@gmail.com>
2023-03-06 23:42:56 -0300

    updating

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -9,16 +9,16 @@ const dog = {
   name: 'Pug',
 };
 
-describe('Dog routes', () => {
+describe('Videogame routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
   beforeEach(() => Dog.sync({ force: true })
     .then(() => Dog.create(dog)));
-  describe('GET /dog', () => {
+  describe('GET /dogs', () => {
     it('should get 200', () =>
-      agent.get('/dog').expect(404)
+      agent.get('/dogs').expect(200)
     );
   });
 });
